c20e9db49caf927ec15e39c955f71b06a9ff86b9
namhyunzi <jhta@DESKTOP-LU4J5LN>
2023-12-27 15:23:25 +0900

    20231227

---
+++ sample/src/main/java/hr/dto/DepartDetailDto.java
@@ -0,0 +1,60 @@
+package hr.dto;
+
+public class DepartDetailDto {
+	
+	private int id;
+	private String name;
+	private int mangerId;
+	private String managerName;
+	private int locationId;
+	private String city;
+	private String streetAddress;
+	
+	public DepartDetailDto() {}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMangerId() {
+		return mangerId;
+	}
+	public void setMangerId(int mangerId) {
+		this.mangerId = mangerId;
+	}
+	public String getManagerName() {
+		return managerName;
+	}
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+	public int getLocationId() {
+		return locationId;
+	}
+	public void setLocationId(int locationId) {
+		this.locationId = locationId;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	
+	
+}
